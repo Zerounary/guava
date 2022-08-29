bb31bf2113905c5352c81693eda8b6c954a8f11f
Zerounary <850657663@qq.com>
2022-08-29 21:28:43 +0800

    使用配置生成 DB_POOL

--- src/db.rs
+++ src/db.rs
@@ -1,11 +1,15 @@
 use std::env;
 
 use lazy_static::lazy_static;
-use sqlx::SqlitePool;
+use sqlx::{sqlite::SqlitePoolOptions, ConnectOptions, SqlitePool};
 
 lazy_static! {
         // connect database
     pub static ref DATABASE_URL: String =
         env::var("DATABASE_URL").expect("No DATABASE_URL provided");
-    pub static ref DB_POOL: SqlitePool = SqlitePool::connect_lazy(DATABASE_URL.as_str()).expect("Could not connect to database");
+    pub static ref DB_POOL: SqlitePool = 
+        SqlitePoolOptions::new()
+            .max_connections(20)
+            .connect_lazy(DATABASE_URL.as_str())
+            .expect("Could not connect to database");
 }
