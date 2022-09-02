25ede686906f34afa8c97ca581ae5f24d77fccc7
Zerounary <850657663@qq.com>
2022-09-03 00:37:44 +0800

    设置统一响应

--- src/server/api/commands/mod.rs
+++ src/server/api/commands/mod.rs
@@ -1,8 +1,34 @@
-pub mod user;
 pub mod hello;
+pub mod user;
+
+use crate::{AppState, server::error::AppError};
+use axum::{response::Json, Extension};
+use std::sync::Arc;
+use serde::{Deserialize, Serialize};
 
 // TODO 用 serde_json::Value 来接所有不知道类型的，又要存起来的数据。 也可以看是否可以用Box
 
 // TODO 用 type 别名来收缩复杂的类型
+
+pub type State = Extension<Arc<AppState>>;
+
+pub type AppResult<T> = Result<Json<Resp<T>>, AppError>;
+
+#[derive(Debug, Default, Serialize, Deserialize)]
+pub struct Resp<T> {
+    code: i32,
+    msg: String,
+    data: Option<T>,
+}
+
+impl<T> Resp<T> {
+    fn ok(data: T) -> AppResult<T> {
+        Ok(Json(Self {
+            code: 0,
+            msg: "success".to_string(),
+            data: Some(data),
+        }))
+    }
+}
 
 // TODO 编写 宏 来收敛重复的代码
--- src/server/api/commands/user.rs
+++ src/server/api/commands/user.rs
@@ -1,10 +1,6 @@
-use std::sync::Arc;
-
-use crate::server::error::AppError;
 use crate::{
-    server::api::model::{CreateUserVO, UpdateUserVO, UserVO},
+    server::{api::model::{CreateUserVO, UpdateUserVO, UserVO}},
     service::uesr_service::{CreateUserInput, UpdateUserInput},
-    AppState,
 };
 use axum::{
     extract::Path,
@@ -13,49 +9,51 @@ use axum::{
     Extension,
 };
 
+use super::{State, Resp, AppResult};
+
 pub async fn users_show_no_cache(
     Path(user_id): Path<i64>,
-    Extension(state): Extension<Arc<AppState>>,
-) -> Result<Json<UserVO>, AppError> {
+    Extension(state): State,
+) -> AppResult<UserVO> {
     let user = state.service.find_no_cache(user_id).await?;
 
-    Ok(Json(user.into()))
+    Resp::ok(user.into())
 }
 
 pub async fn users_show_cache(
     Path(user_id): Path<i64>,
-    Extension(state): Extension<Arc<AppState>>,
-) -> Result<Json<UserVO>, AppError> {
+    Extension(state): State,
+) -> AppResult<UserVO> {
     let user = state.service.find_cache(user_id).await?;
 
-    Ok(Json(user.into()))
+    Resp::ok(user.into())
 }
 
 pub async fn users_show(
     Path(user_id): Path<i64>,
-    Extension(state): Extension<Arc<AppState>>,
-) -> Result<Json<UserVO>, AppError> {
+    Extension(state): State,
+) -> AppResult<UserVO> {
     let user = state.service.find(user_id).await?;
 
-    Ok(Json(user.into()))
+    Resp::ok(user.into())
 }
 
 /// Handler for `POST /users`.
 pub async fn users_create(
     Json(params): Json<CreateUserVO>,
-    Extension(state): Extension<Arc<AppState>>,
-) -> Result<Json<UserVO>, AppError> {
+    Extension(state): State,
+) -> AppResult<UserVO> {
     let service_input = CreateUserInput {
         username: params.username,
     };
     let user = state.service.create(service_input).await?;
 
-    Ok(Json(user.into()))
+    Resp::ok(user.into())
 }
 
 pub async fn users_delete(
     Path(id): Path<i64>,
-    Extension(state): Extension<Arc<AppState>>,
+    Extension(state): State,
 ) -> impl IntoResponse {
     match state.service.delete(id).await {
         Ok(_) => StatusCode::OK,
@@ -66,8 +64,8 @@ pub async fn users_delete(
 pub async fn users_update(
     Path(id): Path<i64>,
     Json(mut user): Json<UpdateUserVO>,
-    Extension(state): Extension<Arc<AppState>>,
-) -> Result<Json<UserVO>, AppError> {
+    Extension(state): State,
+) -> AppResult<UserVO> {
     user.id = Some(id);
     let service_input = UpdateUserInput {
         id,
@@ -75,5 +73,5 @@ pub async fn users_update(
         done: user.done,
     };
     let user = state.service.update(service_input).await?;
-    Ok(Json(user.into()))
+    Resp::ok(user.into())
 }
