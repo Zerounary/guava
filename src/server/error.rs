58383a16c986e0dd2b61583b0e21e9a3bcd67796
Zerounary <850657663@qq.com>
2022-09-03 12:14:30 +0800

    把 业务错误的响应改成和类型同名

--- src/server/error.rs
+++ src/server/error.rs
@@ -8,24 +8,24 @@ use crate::service::uesr_service::UserRepoError;
 use super::api::commands::resp_err;
 
 pub enum AppError {
-    UserRepo(UserRepoError),
+    UserRepoError(UserRepoError),
 }
 
 /// This makes it possible to use `?` to automatically convert a `UserRepoError`
 /// into an `AppError`.
 impl From<UserRepoError> for AppError {
     fn from(inner: UserRepoError) -> Self {
-        AppError::UserRepo(inner)
+        AppError::UserRepoError(inner)
     }
 }
 
 impl IntoResponse for AppError {
     fn into_response(self) -> Response {
         let (status, code, error_message) = match self {
-            AppError::UserRepo(UserRepoError::NotFound) => {
+            AppError::UserRepoError(UserRepoError::NotFound) => {
                 (StatusCode::NOT_FOUND, 1, "User not found")
             }
-            AppError::UserRepo(UserRepoError::InvalidUsername) => {
+            AppError::UserRepoError(UserRepoError::InvalidUsername) => {
                 (StatusCode::UNPROCESSABLE_ENTITY, 2, "Invalid username")
             }
         };
