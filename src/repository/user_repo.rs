b0a91cfd1fb85c6a7540c625a6656eb11897ba8d
Zerounary <850657663@qq.com>
2022-09-05 12:47:33 +0800

    修复 多条件组合查询的问题

--- src/repository/user_repo.rs
+++ src/repository/user_repo.rs
@@ -31,12 +31,13 @@ impl Repository {
 impl_repo_select_one!(UserBO{select_user_by_id});
 impl_repo_select_one!(UserBO{select_user_one(code:&str) => "`where code = #{code}`"});
 impl_repo_select_list!(UserBO{select_user_list(user:UserOptionBO) => 
-    // TODO 此处 py_sql 没有 html_sql 方便，组合条件容易报错，得改
+    // 此处 可以用 py_sql 和 html_sql 对比使用
 r#"
-if user.done != null:
-  `where done = #{user.done}`
-if user.username != null && user.username != '':
-  `where username = #{user.username}`
+where:
+  if user.done != null:
+    `and done = #{user.done}`
+  if user.username != null && user.username != '':
+    `and username = #{user.username}`
   "#});
 
 impl_repo_update!(UserBO{update_user_by_id(id: i64) => "`where id = #{id}`"});
