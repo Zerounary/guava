33ba318b31ba91a116442a070ec9abe8a516381f
Zerounary <850657663@qq.com>
2022-09-08 20:45:07 +0800

    feat: 优雅关机

--- src/main.rs
+++ src/main.rs
@@ -19,6 +19,7 @@ use axum::{
 use rbatis::Rbatis;
 use rbdc_pg::{driver::PgDriver};
 use std::{env, net::SocketAddr, sync::Arc};
+use tokio::signal;
 
 pub struct AppState {
     service: Service,
@@ -60,8 +61,35 @@ async fn main() -> anyhow::Result<()> {
     println!("listening on {}", addr);
     axum::Server::bind(&addr)
         .serve(app.into_make_service())
+        .with_graceful_shutdown(shutdown_signal())
         .await
         .unwrap();
 
     anyhow::Ok(())
+}
+
+async fn shutdown_signal() {
+    let ctrl_c = async {
+        signal::ctrl_c()
+            .await
+            .expect("failed to install Ctrl+C handler");
+    };
+
+    #[cfg(unix)]
+    let terminate = async {
+        signal::unix::signal(signal::unix::SignalKind::terminate())
+            .expect("failed to install signal handler")
+            .recv()
+            .await;
+    };
+
+    #[cfg(not(unix))]
+    let terminate = std::future::pending::<()>();
+
+    tokio::select! {
+        _ = ctrl_c => {},
+        _ = terminate => {},
+    }
+
+    println!("signal received, starting graceful shutdown");
 }
